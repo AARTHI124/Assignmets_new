a5dae49ade273e55c668da44e14b798f67508e12
AARTHI124 <140309364+AARTHI124@users.noreply.github.com>
2025-11-04 00:28:15 +0530

    push

---
+++ Day-22/Task1.js
@@ -0,0 +1,7 @@
+//Create an object book with title, author, and pages.
+let book= {
+    title: "Warmth",
+    author: "Rithvik Singh",
+    pages:198
+};
+console.log(book);
---
+++ Day-22/Task4.js
@@ -0,0 +1,15 @@
+//Create a class Employee with name and salary — add a method to display info.
+class Employee{
+    constructor(name,salery){
+        this.name=name;
+        this.salery=salery;
+    }
+    display(){
+        console.log(`Employee Name: ${this.name}`);
+        console.log(`Employee salery:${this.salery}`);        
+    }
+}
+const emp1= new Employee("Aarthi Vaasugi",50000);
+const emp2 = new Employee("Keerthi",100000);
+emp1.display();
+emp2.display();
