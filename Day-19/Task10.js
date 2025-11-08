53761fab455ae64be25e5f8df9badc17bb0f3e69
AARTHI124 <140309364+AARTHI124@users.noreply.github.com>
2025-11-08 22:30:01 +0530

    push

--- Day-19/Task10.js
+++ Day-19/Task10.js
@@ -1,5 +1,5 @@
 //Use break to stop the loop when a specific number (say 10) is reached.
-for(i=1;i<=10;i++){
-    if(i==10) break
+for(i=1;i<=20;i++){
+    if(i==6) break
     console.log(i);
 }
--- Day-21/Task3.js
+++ Day-21/Task3.js
@@ -1,5 +1,5 @@
 //Create an array of numbers and print all using forEach().
 let colors = ["Red", "Green", "Blue","black","white"];
-colors.forEach(function(col){
- console.log(col);
+colors.forEach(function (col){
+ console.log (col);
 })
